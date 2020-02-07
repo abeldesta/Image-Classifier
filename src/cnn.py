964359f4dea59123ff6c8d2634c36acd2ce048dd
Abel Desta <abeld@ksu.edu>
2020-02-06 21:30:56 -0700

    EDA plots and model update remove dropout

--- src/cnn.py
+++ src/cnn.py
@@ -95,7 +95,7 @@ def define_model(nb_filters, kernel_size, input_shape, pool_size):
 
 if __name__ == "__main__":
     nb_classes = 3 
-    nb_epoch = 20    
+    nb_epoch = 10    
     img_rows, img_cols = 100, 100
     input_shape = (img_rows, img_cols, 3)
     nb_filters = 32
