c77d58773275d2cb1b74094c7c057e113839008f
Abel Desta <abeld@ksu.edu>
2020-02-09 22:23:52 -0700

    new artists

--- src/clean.py
+++ src/clean.py
@@ -12,7 +12,7 @@ np.random.seed(1337)  # for reproducibility
 home = os.getcwd()
 
 train_loc = 'data/Train/'
-artist = ['Vincent_Van_Gogh', 'Edgar_Degas', 'Pablo_Picasso']
+artist = ['Andy_Warhol', 'Paul_Klee', 'Mikhail_Vrubel']
 train_dict = {}
 for i in artist:
     os.chdir(os.path.abspath(train_loc + i))
