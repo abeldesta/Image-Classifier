c3f2c10dca07480a274f1924bd5c71349c8e989c
Abel Desta <abeld@ksu.edu>
2020-02-09 23:07:34 -0700

    4 layer conv model changed batch size

--- src/cnn.py
+++ src/cnn.py
@@ -27,21 +27,21 @@ def define_model(nb_filters, kernel_size, input_shape, pool_size):
                         input_shape=input_shape, name = 'conv_layer2'))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=pool_size, name = 'pool_layer2'))
- 
+    model.add(Dropout(0.5))
 
     model.add(Conv2D(nb_filters, (kernel_size[0], kernel_size[1]),
                         padding='valid', 
                         input_shape=input_shape, name = 'conv_layer3'))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=pool_size, name = 'pool_layer3'))
-  
+    model.add(Dropout(0.5))
 
-    model.add(Conv2D(nb_filters, (kernel_size[0], kernel_size[1]),
+    model.add(Conv2D(nb_filters*2, (kernel_size[0], kernel_size[1]),
                         padding='valid', 
                         input_shape=input_shape, name = 'conv_layer4'))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=pool_size, name = 'pool_layer4'))
-    model.add(Dropout(0.8))
+    model.add(Dropout(0.5))
 
     model.add(Flatten())
     print('Model flattened out to ', model.output_shape)
@@ -49,7 +49,7 @@ def define_model(nb_filters, kernel_size, input_shape, pool_size):
     model.add(Dense(128)) 
     model.add(Activation('relu'))
 
-    model.add(Dropout(0.6))
+    model.add(Dropout(0.65))
 
     model.add(Dense(nb_classes))
     model.add(Activation('softmax'))
