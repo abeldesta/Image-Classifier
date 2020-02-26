b561d4f2541026a402b90a760abca3dc6c61da6a
Abel Desta <abeld@ksu.edu>
2020-02-25 21:14:51 -0700

    Ensemble and CNN model update

--- src/ensemble.py
+++ src/ensemble.py
@@ -20,7 +20,7 @@ rf = RandomForestClassifier(n_estimators=100,
                         bootstrap=True, 
                         oob_score=False, 
                         n_jobs=-1, 
-                        random_state=None, 
+                        random_state=531, 
                         verbose=0, 
                         warm_start=False, 
                         class_weight=None)
