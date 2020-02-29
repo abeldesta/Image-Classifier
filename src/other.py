0761e413acd415230c4b019702a0ba74117e9baf
Abel Desta <abeld@ksu.edu>
2020-02-28 23:01:39 -0700

    model summary cnn_model

--- src/other.py
+++ src/other.py
@@ -100,11 +100,11 @@ for i, ax in enumerate(axs.flatten()):
 plt.savefig('img/misclassified_imgs.png', bbox_inches='tight')
 plt.tight_layout()
 
-fig, axs = plt.subplots(1,3)
+fig, axs = plt.subplots(1,3,, fig_size =(3,3))
 for i, ax in enumerate(axs.flatten()):
     xtickLocations = np.arange(len(labels))
     data = plot_probs[i]
-    ax.barh(xtickLocations, data, 0.5)
+    ax.bar(xtickLocations, data, 0.5)
     ax.set_yticks(xtickLocations)
     for i, p in enumerate(data):
         ax.annotate(f'{p*100:0.1f}%', (p + 0.005, i))
