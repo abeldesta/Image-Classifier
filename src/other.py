c3efd2a52cead1543719dae22de5f1ca474f46b2
Abel Desta <abeld@ksu.edu>
2020-02-29 00:03:50 -0700

    adjusting bar plot

--- src/other.py
+++ src/other.py
@@ -100,8 +100,9 @@ for i, ax in enumerate(axs.flatten()):
 plt.savefig('img/misclassified_imgs.png', bbox_inches='tight')
 plt.tight_layout()
 
-fig, axs = plt.subplots(1,3)
-for i, ax in enumerate(axs.flatten()):
+
+for i in np.arange(len(labels)):
+    fig, ax = plt.subplots(1,1)
     xtickLocations = np.arange(len(labels))
     data = plot_probs[i]
     ax.bar(xtickLocations, data, 0.5)
@@ -111,6 +112,6 @@ for i, ax in enumerate(axs.flatten()):
     plt.xticks(rotation= 85)
     for i, p in enumerate(data):
         ax.annotate(f'{p*100:0.1f}%', (i, p))
-plt.savefig('img/misclassified_probs.png')
+    plt.savefig('img/misclassified_probs{}.png'.format(i))
 
 
